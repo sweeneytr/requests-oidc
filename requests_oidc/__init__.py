57318a19acb4e6ac092d341330d36ae578e5208c
Tristan Sweeney <tsweeney@dustidentity.com>
2023-02-23 15:36:15 -0500

    Disable OAuthLib by manipulating env during init
    Man that lib is hairy

--- requests_oidc/__init__.py
+++ requests_oidc/__init__.py
@@ -1,2 +1,5 @@
+import os
 from .factory import (make_client_credentials_session, make_oidc_session,
                       make_os_cached_session, make_path_session)
+
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = 'True'
